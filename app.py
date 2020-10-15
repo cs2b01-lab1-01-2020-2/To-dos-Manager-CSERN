60c663671303994e456700fe880a3e48321a3352
diegor8 <diegorosario2014@gmail.com>
2020-10-15 11:29:38 -0500

    postgres instead of sqlite

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy import ForeignKey, Integer, String
 
 def inciar():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:////tmp/test.db'
     db = SQLAlchemy(app)
 
     return app, db
