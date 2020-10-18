a8ccfd4201add2432b53d5027bcdd1d36a94b4ea
renatoseb <renatorodriguezllanos@gmail.com>
2020-10-17 19:21:59 -0500

    front clean

--- templates/login.js
+++ templates/login.js
@@ -1,4 +1,68 @@
-const form = new FormData(form)
+function validEmail(element) {
+	var text = document.getElementById(element.id).value;
+	var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+	if (!regex.test(text)) {
+		document.getElementById("bademail").className = "error uk-animation-shake";
+	}
+	else{
+	  document.getElementById("bademail").className = "hidden";
+	}
+};
+
+
+document.getElementById('login').onsubmit = function(e){
+  e.preventDefault();
+
+  fetch('XXXXXX',{
+	  method: 'POST',
+	  // Capturamos la data del form
+	  body: JSON.stringify({
+		  // Este es el javascript object
+		  'user': document.getElementById('user').value,
+		  'password': document.getElementById('password').value
+	  }),
+	  headers: {
+		  'content-type': 'application/json'
+	  }
+  })
+	  .then(function(response){
+		  return response.json();
+	  })
+	  .then(function(jsonResponse){
+		  const user_ = document.getElementById('user');
+		  
+		  if( user_ in jsonResponse['username_name']){
+			  document.getElementById('baduser').className = 'hidden';
+		  }
+		  else{
+			  document.getElementById('baduser').className = 'error';
+		  }
+
+		  if( user_ in jsonResponse['password']){
+			  document.getElementById('baduser').className = 'hidden';
+		  }
+		  else{
+			  document.getElementById('baduser').className = 'error';
+		  }
+	  })
+	  .catch(function(error){
+		  document.getElementById('baduser').className = '';
+	  });
+}
+
+/*
+  try:
+	  user = db_session.query(entities.User
+		  ).filter(entities.User.username == username
+		  ).filter(entities.User.password == password
+		  ).one()
+	  return Response(json.dumps(user,cls=connector.AlchemyEncoder), status=200, mimetype='application/json')
+  except Exception:
+	  message = {'message': 'Unauthorized'}
+	  return Response(message, status=401, mimetype='application/json')
+*/
+/*const form = new FormData(form)
 const user = {
     user,
     password
@@ -54,4 +118,4 @@ document.getElementById('createuser').onsubmit = function(e){
 	.catch(function(error){
 		document.getElementById('baduser').className = '';
 	});
-}
+}*/
