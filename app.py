4f7e775b97a9c05351428978ccb1df04b49ab876
MistyBlunch <gracenikole@gmail.com>
2020-10-19 15:00:41 -0500

    back updated

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from wtforms_fields import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rrodriguez:neoscience30@localhost:5432/todosdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mistyblunch:pvta@localhost:5432/todosdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -38,38 +40,68 @@ class Category(db.Model):
 	name = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/user/create', methods=['GET'])
-def get_user():
-	print("get_user")
-	username = request.args.get('username_reg', '')
-	email = request.args.get('email', '')
-	password = request.args.get('password_reg', '')
-	user = User(username=username, email=email, password=password)
-	db.session.add(user)
-	db.session.commit()
-	
-	return redirect(url_for('login'))
+# Register
+@app.route('/auth/signup', methods=['POST'])
+def signup():
 
+	reg_form = RegistrationForm()
 
-@app.route('/user/create', methods=['POST'])
-def post_user():
-	print('post_user')
+	print('signup')
 	try:
 		username = request.get_json()['username']
 		email = request.get_json()['email']
 		password = request.get_json()['password']
-		user = User(username=username, email=email, password=password)
-		db.session.add(user)
-		db.session.commit()
-		return jsonify({
-			'username': user.username,
-			'email': user.email,
-			'password': user.password,
-		})
+
+		usxr = User.query.filter_by(username=username).first()
+		eml = User.query.filter_by(email=email).first()
+	
+		if usxr:
+			flash('User already exists')
+			return render_template('login.html')
+		elif eml:
+			flash('Email address already exists')
+			return render_template('login.html')
+		else:
+			user = User(username=username, email=email, password=password)
+			db.session.add(user)
+			db.session.commit()
+			return jsonify({
+				'username': user.username,
+				'email': user.email,
+				'password': user.password,
+			})
 	except Exception as e:
 		db.session.rollback()
 	finally:
 		db.session.close()
+
+
+# Login
+# @app.route('/autth/login', methods=['GET'])
+# def login():
+# 	print('login')
+# 	try:
+# 		description = request.args.get('description', '')
+# 		password = request.get_json()['password']
+
+# 		usxr = User.query.filter_by(username=username).first()
+# 		pwd = User.query.filter_by(password=password).first()
+
+# 		print(usxr)
+# 		print(pwd)
+	
+# 		if usxr and pwd:
+# 			return render_template('index.html')
+# 		else:
+# 			return 'no logeado'
+# 	except Exception as e:
+# 		return 'no logeado'
+
+
+@app.route('/todos')
+def todos():
+	#todos = Todos.query.all()
+	return render_template('todos.html')
 
 @app.route('/')
 def index():
