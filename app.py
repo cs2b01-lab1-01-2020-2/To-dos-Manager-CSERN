a81bf598fefe36f838765a3689c538f53e84b014
1v1menub <ignacio.rubio@utec.edu.pe>
2020-10-22 18:09:17 -0500

    xdnomas

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 from modelos import * 
 
-connection = connection = psycopg2.connect(database = "todosdb", user = "postgres", password = "robior123",)
+connection = connection = psycopg2.connect(database = "todosdb", user = "postgres", password = "robior123")
 cursor = connection.cursor()
 
 # Register
