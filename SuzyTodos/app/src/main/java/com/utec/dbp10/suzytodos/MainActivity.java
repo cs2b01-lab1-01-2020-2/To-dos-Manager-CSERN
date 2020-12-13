5fae0def9c6679a4ab0088e208bf1e3a90e2817f
1v1menub <ignacio.rubio@utec.edu.pe>
2020-12-12 19:17:05 -0500

    ANDROIRD VIEW

---
+++ SuzyTodos/app/src/main/java/com/utec/dbp10/suzytodos/MainActivity.java
@@ -0,0 +1,21 @@
+package com.utec.dbp10.suzytodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirtodos(View veiw) {
+        Intent intent = new Intent(this, todos.class);
+        startActivity(intent);
+    }
+}
---
+++ SuzyTodos/app/src/main/java/com/utec/dbp10/suzytodos/todos.java
@@ -0,0 +1,60 @@
+package com.utec.dbp10.suzytodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class todos extends AppCompatActivity {
+
+    ArrayList<String> todos;
+    ArrayAdapter<String> todosAdapter;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos);
+
+        listView = findViewById(R.id.list);
+        todos = new ArrayList<>();
+        todosAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, todos);
+        listView.setAdapter(todosAdapter);
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Todo deleted", Toast.LENGTH_LONG).show();
+
+                todos.remove(position);
+                todosAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    public void addTodo(View view) {
+        EditText adder = findViewById(R.id.adder);
+        String todoDescription = adder.getText().toString();
+
+        if(!(todoDescription.equals(""))) {
+            todosAdapter.add(todoDescription);
+            adder.setText("");
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Todo needs a description", Toast.LENGTH_LONG).show();
+        }
+    }
+}
